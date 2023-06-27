e87d66a4a7656033715b40cf0c3155eec5717fa5
ashanfernando <ashan256@gmail.com>
2023-06-27 16:31:24 +0530

    Test

--- dist/index.js
+++ dist/index.js
@@ -3969,7 +3969,7 @@ async function run(exec, wsdir) {
   await exec(`npm config set //node-registry.bit.cloud/:_authToken $BIT_TOKEN`);
 
   // bit install dependencies
-  await exec(`cd ${wsdir} && bit install`);
+  await exec(`cd tests && bit install`);
 }
 
 module.exports = run;
