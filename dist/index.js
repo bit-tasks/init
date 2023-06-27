26469c7c462dc9dbb0041f7bf5175fb3298c2956
ashanfernando <ashan256@gmail.com>
2023-06-27 15:31:58 +0530

    Update dist

--- dist/index.js
+++ dist/index.js
@@ -4132,6 +4132,8 @@ const run = __nccwpck_require__(73);
 
 try {
   const wsDir = core.getInput('ws-dir');
+  console.log("#####");
+  console.log(wsDir)
   run(exec, wsDir);
 } catch (error) {
   core.setFailed(error.message);
