f5c18cce456ce1a1029918739d8f9beff293202b
ashanfernando <ashan256@gmail.com>
2023-06-27 15:31:44 +0530

    Test

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const run = require("./scripts/init");
 
 try {
   const wsDir = core.getInput('ws-dir');
+  console.log("#####");
+  console.log(wsDir)
   run(exec, wsDir);
 } catch (error) {
   core.setFailed(error.message);
