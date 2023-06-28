375e22b48b6ef8e268a429346328ecaba3e4fbff
ashanfernando <ashan256@gmail.com>
2023-06-28 12:43:10 +0530

    Debug

--- index.ts
+++ index.ts
@@ -1,20 +1,17 @@
-import * as fs from "fs";
-import * as core from "@actions/core";
-import run from "./scripts/init";
+import * as fs from 'fs';
+import * as core from '@actions/core';
+import { exec } from '@actions/exec';
+import run, { ExecFunction } from './scripts/init';
 
 try {
-  const wsDir: string = core.getInput("ws-dir");
-  run(wsDir);
-  // Set wsDir path for subsequent steps in GitHub Actions
-  fs.appendFileSync(
-    process.env.GITHUB_ENV as string,
-    `WSDIR="${process.env.WSDIR}"`
-  );
-  // Set Bit path for subsequent steps in GitHub Actions
-  fs.appendFileSync(
-    process.env.GITHUB_PATH as string,
-    process.env.PATH as string
-  );
+  const wsDir: string = core.getInput('ws-dir');
+  const stdExec: ExecFunction = (command: string, options?: {cwd: string}): Promise<number> => exec(command, [], options);
+  run(stdExec, wsDir).then((): void => {
+    // Set wsDir path for subsequent steps in GitHub Actions
+    fs.appendFileSync(process.env.GITHUB_ENV as string, `WSDIR="${process.env.WSDIR}"`);
+    // Set Bit path for subsequent steps in GitHub Actions
+    fs.appendFileSync(process.env.GITHUB_PATH as string, process.env.PATH as string);
+  });
 } catch (error: any) {
   core.setFailed(error.message);
 }
--- scripts/init.ts
+++ scripts/init.ts
@@ -1,44 +1,38 @@
-import * as fs from "fs";
-import * as path from "path";
-import { execSync } from "child_process";
+import * as fs from 'fs';
+import * as path from 'path';
 
-const run: (wsdir: string) => void = (wsdir) => {
+export type ExecFunction = (command: string, options?: {cwd: string}) => Promise<number>;
+
+const run: (exec: ExecFunction, wsdir: string) => Promise<void> = async (exec, wsdir) => {
   // get bit version to install
   const wsDirPath = path.resolve(wsdir);
   // sets wsdir env for any external usage
   process.env.WSDIR = wsdir;
-
+  
   const wsFile = path.join(wsDirPath, "workspace.jsonc");
   const workspace = fs.readFileSync(wsFile).toString();
   const match = /"engine": "(.*)"/.exec(workspace);
   const bitEngineVersion = match ? match[1] : "";
 
   // install bvm globally
-  execSync("npm i -g @teambit/bvm", { stdio: "inherit" });
+  await exec("npm i -g @teambit/bvm");
   // install bit
-  execSync(`bvm install ${bitEngineVersion} --use-system-node`, {
-    stdio: "inherit",
-  });
+  await exec(`bvm install ${bitEngineVersion} --use-system-node`);
   // sets path for current step
   process.env.PATH = `${process.env.HOME}/bin:` + process.env.PATH;
 
   // config bit/npm for CI/CD
-  execSync("bit config set interactive false", { stdio: "inherit" });
-  execSync("bit config set analytics_reporting false", { stdio: "inherit" });
-  execSync("bit config set anonymous_reporting false", { stdio: "inherit" });
-  execSync("bit config set user.token $BIT_TOKEN", { stdio: "inherit" });
-
-  // set your npmrc
-  execSync(`npm config set always-auth true`, { stdio: "inherit" });
-  execSync(`npm config set @teambit:registry https://node-registry.bit.cloud`, {
-    stdio: "inherit",
-  });
-  execSync(`npm config set //node-registry.bit.cloud/:_authToken $BIT_TOKEN`, {
-    stdio: "inherit",
-  });
+  await exec("bit config set interactive false");
+  await exec("bit config set analytics_reporting false");
+  await exec("bit config set anonymous_reporting false");
+  await exec("bit config set user.token $BIT_TOKEN");
+  await exec(`npm config set always-auth true`);
+  //TODO: move these back to "node.bit.cloud" once that promotion occurs
+  await exec(`npm config set @teambit:registry https://node-registry.bit.cloud`);
+  await exec(`npm config set //node-registry.bit.cloud/:_authToken $BIT_TOKEN`);
 
   // bit install dependencies
-  execSync("bit install --add-missing-deps", { stdio: "inherit", cwd: wsdir });
-};
+  await exec("bit install --add-missing-deps", { cwd: wsdir });
+}
 
 export default run;
