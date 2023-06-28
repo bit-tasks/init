703013b243076d6eb2a284a7dc6827e91c441a62
ashanfernando <ashan256@gmail.com>
2023-06-28 14:00:05 +0530

    Updated

--- dist/index.js
+++ dist/index.js
@@ -4031,7 +4031,7 @@ const run = (exec, wsdir) => __awaiter(void 0, void 0, void 0, function* () {
     // get bit version to install
     const wsDirPath = path.resolve(wsdir);
     // sets wsdir env for any external usage
-    process.env.WSDIR = path.relative(process.cwd(), wsDirPath);
+    process.env.WSDIR = wsdir;
     const wsFile = path.join(wsDirPath, "workspace.jsonc");
     const workspace = fs.readFileSync(wsFile).toString();
     const match = /"engine": "(.*)"/.exec(workspace);
--- scripts/init.ts
+++ scripts/init.ts
@@ -7,7 +7,7 @@ const run: (exec: ExecFunction, wsdir: string) => Promise<void> = async (exec, w
   // get bit version to install
   const wsDirPath = path.resolve(wsdir);
   // sets wsdir env for any external usage
-  process.env.WSDIR = path.relative(process.cwd(), wsDirPath);
+  process.env.WSDIR = wsdir;
   
   const wsFile = path.join(wsDirPath, "workspace.jsonc");
   const workspace = fs.readFileSync(wsFile).toString();
