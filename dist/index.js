0f1abbe63b1ae02da62c586c2a172c2da52ace7a
ashanfernando <ashan256@gmail.com>
2023-06-28 15:20:29 +0530

    Update init

--- dist/index.js
+++ dist/index.js
@@ -4047,10 +4047,10 @@ const run = (exec, wsdir) => __awaiter(void 0, void 0, void 0, function* () {
     yield exec("bit config set analytics_reporting false");
     yield exec("bit config set anonymous_reporting false");
     yield exec("bit config set user.token $BIT_TOKEN");
-    yield exec(`npm config set always-auth true`);
+    yield exec("npm config set always-auth true");
     //TODO: move these back to "node.bit.cloud" once that promotion occurs
-    yield exec(`npm config set '@bit:registry' https://node-registry.bit.cloud`);
-    yield exec(`RUN npm config set '@teambit:registry' https://node-registry.bit.cloud`);
+    yield exec("npm config set '@bit:registry' https://node-registry.bit.cloud");
+    yield exec("RUN npm config set '@teambit:registry' https://node-registry.bit.cloud");
     // bit install dependencies
     yield exec("bit install --add-missing-deps", { cwd: wsdir });
 });
--- scripts/init.ts
+++ scripts/init.ts
@@ -26,10 +26,10 @@ const run: (exec: ExecFunction, wsdir: string) => Promise<void> = async (exec, w
   await exec("bit config set analytics_reporting false");
   await exec("bit config set anonymous_reporting false");
   await exec("bit config set user.token $BIT_TOKEN");
-  await exec(`npm config set always-auth true`);
+  await exec("npm config set always-auth true");
   //TODO: move these back to "node.bit.cloud" once that promotion occurs
-  await exec(`npm config set '@bit:registry' https://node-registry.bit.cloud`);
-  await exec(`RUN npm config set '@teambit:registry' https://node-registry.bit.cloud`);
+  await exec("npm config set '@bit:registry' https://node-registry.bit.cloud");
+  await exec("RUN npm config set '@teambit:registry' https://node-registry.bit.cloud");
 
   // bit install dependencies
   await exec("bit install --add-missing-deps", { cwd: wsdir });
