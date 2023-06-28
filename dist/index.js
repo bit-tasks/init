fb65a7f3d8fedff29e79d3514813aec0d368a27d
ashanfernando <ashan256@gmail.com>
2023-06-28 09:27:26 +0530

    debug

--- dist/index.js
+++ dist/index.js
@@ -3982,6 +3982,9 @@ module.exports = run;
 async function setBitPath(exec) {
   // sets path for subsequent steps
   await exec(`echo "$HOME/bin" >> $GITHUB_PATH`);
+  const bitPath = await exec('where bit');
+  console.log(bitPath);
+  console.log("###")
 }
 
 module.exports = setBitPath;
--- scripts/github.js
+++ scripts/github.js
@@ -1,6 +1,9 @@
 async function setBitPath(exec) {
   // sets path for subsequent steps
   await exec(`echo "$HOME/bin" >> $GITHUB_PATH`);
+  const bitPath = await exec('where bit');
+  console.log(bitPath);
+  console.log("###")
 }
 
 module.exports = setBitPath;
