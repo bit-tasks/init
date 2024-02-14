d38c3da89dc23846029f56da223632e0d7d42dc0
Jonathan Stevens <jonathan.stevens@eventiva.co.uk>
2024-02-14 14:20:08 +0000

    chore(action.yml): add optional inputs for Github token, Bit cloud access token, Git user name, and Git user email to provide flexibility in configuration fix(index.ts): fix linting issues by adding proper indentation and line breaks for better code readability fix(init.ts): fix linting issues by adding proper indentation and line breaks for better code readability feat(init.ts): add support for optional inputs for Git user name, Git user email, Bit cloud access token, and Github token to provide flexibility in configuration

--- scripts/init.ts
+++ scripts/init.ts
@@ -1,41 +1,52 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as jsoncParser from 'jsonc-parser';
-import { exec } from "@actions/exec";
+import * as fs from 'fs'
+import * as path from 'path'
+import * as jsoncParser from 'jsonc-parser'
+import core from '@actions/core'
+import { exec } from '@actions/exec'
 
-const run = async (wsdir: string) => {
-  const wsDirPath = path.resolve(wsdir);
+/**
+ *
+ */
+const run = async (wsdir: string): Promise<void> => {
+  const wsDirPath = path.resolve(wsdir)
 
   // sets wsdir env for dependent tasks usage
-  process.env.WSDIR = wsdir;
+  process.env.WSDIR = wsdir
+  process.env.GIT_USER_NAME =
+    process.env.GIT_USER_NAME ?? core.getInput('GIT_USER_NAME')
+  process.env.GIT_USER_EMAIL =
+    process.env.GIT_USER_EMAIL ?? core.getInput('GIT_USER_EMAIL')
+  process.env.BIT_CLOUD_ACCESS_TOKEN =
+    process.env.BIT_CLOUD_ACCESS_TOKEN ??
+    core.getInput('BIT_CLOUD_ACCESS_TOKEN')
+  process.env.GITHUB_TOKEN = process.env.GITHUB_TOKEN ?? core.getInput('token')
 
-  const wsFile = path.join(wsDirPath, "workspace.jsonc");
-  const workspace = fs.readFileSync(wsFile).toString();
+  const wsFile = path.join(wsDirPath, 'workspace.jsonc')
+  const workspace = fs.readFileSync(wsFile).toString()
 
   // sets org and scope env for dependent tasks usage
-  const workspaceObject = jsoncParser.parse(workspace);
+  const workspaceObject = jsoncParser.parse(workspace)
   const defaultScope =
-    workspaceObject["teambit.workspace/workspace"].defaultScope;
-  const [Org, Scope] = defaultScope.split(".");
-  process.env.ORG = Org;
-  process.env.SCOPE = Scope;
+    workspaceObject['teambit.workspace/workspace'].defaultScope
+  const [Org, Scope] = defaultScope.split('.')
+  process.env.ORG = Org
+  process.env.SCOPE = Scope
 
   // install bvm and bit
-  const bitEngineVersion = 
-    workspaceObject["teambit.harmony/bit"]?.engine || "";
+  const bitEngineVersion = workspaceObject['teambit.harmony/bit']?.engine || ''
 
-  await exec("npm i -g @teambit/bvm");
-  await exec(`bvm install ${bitEngineVersion} --use-system-node`);
+  await exec('npm i -g @teambit/bvm')
+  await exec(`bvm install ${bitEngineVersion} --use-system-node`)
   // sets path for current step
-  process.env.PATH = `${process.env.HOME}/bin:` + process.env.PATH;
+  process.env.PATH = `${process.env.HOME}/bin:${process.env.PATH}`
 
   // config bit/npm for CI/CD
-  process.env.BIT_CONFIG_ANALYTICS_REPORTING = "false";
-  process.env.BIT_CONFIG_ANONYMOUS_REPORTING = "false";
-  process.env.BIT_CONFIG_INTERACTIVE = "false";
+  process.env.BIT_CONFIG_ANALYTICS_REPORTING = 'false'
+  process.env.BIT_CONFIG_ANONYMOUS_REPORTING = 'false'
+  process.env.BIT_CONFIG_INTERACTIVE = 'false'
 
   // bit install dependencies
-  await exec("bit install", [], { cwd: wsdir });
-};
+  await exec('bit install', [], { cwd: wsdir })
+}
 
-export default run;
+export default run
